ffd88aff0090cea5ab8ab011efd478cbba388c20
mmm832007 <mmm832007@gmail.com>
2024-08-01 19:54:44 +0800

    Create ex_2-3.cpp

---
+++ ex_2-3.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main() {
+    int a, b, product;
+    a = 20;
+    b = 7;
+
+ 
+    // Calculating product
+    product = a * b;
+
+ 
+    printf("Product = %d", product);
+    
+    return 0;
+}
